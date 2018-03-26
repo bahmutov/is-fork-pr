29d73f0da7dd8848652b5d9dd1c5091835528853
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-25 22:18:12 -0400

    chore: initial

---
+++ src/is-fork-pr-spec.js
@@ -0,0 +1,10 @@
+'use strict'
+
+/* eslint-env mocha */
+const isForkPr = require('.')
+
+describe('is-fork-pr', () => {
+  it('write this test', () => {
+    console.assert(isForkPr, 'should export something')
+  })
+})
