1c62dad288cbfebd41b06d810efb2ab0211ada76
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2018-03-25 22:46:14 -0400

    fix: correct comparison logic

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 'use strict'
 
+/**
+ * Returns true if the Travis CI is building a pull request from
+ * a remote repository fork. This means the environment variables
+ * are NOT set.
+ */
 const isForkPrTravis = () => {
   if (!process.env.TRAVIS_PULL_REQUEST) {
     return false
   }
-  return process.env.TRAVIS_REPO_SLUG === process.env.TRAVIS_PULL_REQUEST_SLUG
+  return process.env.TRAVIS_REPO_SLUG !== process.env.TRAVIS_PULL_REQUEST_SLUG
 }
 
 const isForkPr = isForkPrTravis
